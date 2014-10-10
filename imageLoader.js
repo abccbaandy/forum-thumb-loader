1b5c60fc42f31dae241962161412b2a046465c60
Leou <abccbaandy@gmail.com>
2014-10-10 17:17:03 +0800

    skip when no image

--- imageLoader.js
+++ imageLoader.js
@@ -1,5 +1,5 @@
 var imageSpaces = document.querySelectorAll(".tr3.t_one td a[title='打开新窗口']");
-var imageUrls = document.querySelectorAll(".tr3.t_one td h3 a");
+var postUrls = document.querySelectorAll(".tr3.t_one td h3 a");
 var skipUrls = document.querySelectorAll(".tr3.t_one td img[title='置顶帖标志']");
 
 function showImg(img, imageSpacesIndex) {
@@ -14,6 +14,8 @@ function loadImg(imgsUrls, loadImgCount, imageSpacesIndex) {
     var img = new Image();
     img.src = imgsUrls[loadImgCount].src;
     img.onload = function() {
+
+        //we only care the first valid image:)
         if(img.naturalWidth>100&&img.naturalHeight>100) {
             showImg(img, imageSpacesIndex);
         }
@@ -30,12 +32,16 @@ function getImg (responseText, imageSpacesIndex) {
     var result = new DOMParser().parseFromString(responseText, "text/html");
 
     var imgsUrls = result.querySelectorAll("#read_tpc.f14 img");
-    var loadImgCount = 0;
-    loadImg(imgsUrls, loadImgCount, imageSpacesIndex);
+
+    //check there is at least one image
+    if(imgsUrls.length!=0) {
+        var loadImgCount = 0;
+        loadImg(imgsUrls, loadImgCount, imageSpacesIndex);
+    }
 }
 
 
-function httpGet(theUrl, showImgIndex)
+function httpGet(url, showImgIndex)
 {
 	xmlhttp=new XMLHttpRequest();
     xmlhttp.onreadystatechange=function()
@@ -43,14 +49,13 @@ function httpGet(theUrl, showImgIndex)
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
         	getImg(xmlhttp.responseText, showImgIndex);
-            //return xmlhttp.responseText;
         }
     }
-    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.open("GET", url, false );
     xmlhttp.send();    
 }
 
-for (var i = skipUrls.length; i < imageUrls.length; i++) { 
+for (var i = skipUrls.length; i < postUrls.length; i++) { 
 //for (var i = skipUrls.length+16; i < skipUrls.length+25; i++) { 
-	httpGet(imageUrls[i].href, i);
+	httpGet(postUrls[i].href, i);
 }
