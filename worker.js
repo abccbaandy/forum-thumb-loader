a4a97aaa491f03420f511c896f56362eaac7d41a
Leou <abccbaandy@gmail.com>
2014-10-12 15:17:32 +0800

    delete nouse file(worker.js)

--- worker.js
+++
@@ -1,8 +0,0 @@
-postMessage('Hello from worker!');
-// Example: cross-origin XMLHttpRequest using permissions from the manifest file.
-var x = new XMLHttpRequest();
-x.open('GET', 'http://www.cwb.gov.tw/V7/index.htm');
-x.onload = function() {
-    postMessage('Result\n' + x.responseText);
-};
-x.send();
