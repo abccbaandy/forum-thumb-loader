a45d8f7bedf1c536d7257bc8852e8fdd462a4e99
Leou <abccbaandy@gmail.com>
2014-10-14 00:35:08 +0800

    clean code

--- eventPage.js
+++ eventPage.js
@@ -4,7 +4,6 @@ function dbOpenCallback() {
     console.log("dbOpenCallback : " + todoDB);
     todoDB.fetchTodos(function (todos) {
     	console.log("fetchTodos todos.length: " + todos.length);
-    	//sendResponseLocal({data: "666"});
         sendResponseLocal({data: todos});
     });
 }
@@ -13,16 +12,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method == "getData") {
     	sendResponseLocal = sendResponse;
     	todoDB.open(dbOpenCallback);
-    	//sendResponseLocal({data: "999"});
-    	/*todoDB.open(function () {
-    		console.log("dbOpenCallback : " + todoDB);
-    		todoDB.fetchTodos(function (todos) {
-    			console.log("fetchTodos todos.length: " + todos.length);
-    			sendResponse({data: "666"});
-    		});
-		});*/
         return true;
     }
     else
-      sendResponse({}); // snub them.
+      sendResponse({}); // nothing to do:)
 });
